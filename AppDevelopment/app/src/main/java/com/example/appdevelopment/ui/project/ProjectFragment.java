e50c3f477c468fb07b0ea2821ba4ab5b87443e51
taewankang <king6096211@naver.com>
2019-11-05 19:29:13 +0900

    2019.11.05

--- AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/ProjectFragment.java
+++ AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/ProjectFragment.java
@@ -1,15 +1,11 @@
 package com.example.appdevelopment.ui.project;
 
-<<<<<<< HEAD
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-=======
->>>>>>> 195a4d0b793c9c40d5a52b9017fb41357775f937
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,17 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-=======
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
->>>>>>> 195a4d0b793c9c40d5a52b9017fb41357775f937
 
 import com.example.appdevelopment.R;
 
 public class ProjectFragment extends Fragment {
-<<<<<<< HEAD
     Button button;
     Bundle bundle;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,10 +29,6 @@ public class ProjectFragment extends Fragment {
 
             }
         });
-=======
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_project, container, false);
->>>>>>> 195a4d0b793c9c40d5a52b9017fb41357775f937
         return view;
     }
 }
