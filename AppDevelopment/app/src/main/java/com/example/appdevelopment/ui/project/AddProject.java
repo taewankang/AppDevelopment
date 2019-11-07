74eb39026328a2525f7568ba0efd0ed068f45107
taewankang <king6096211@naver.com>
2019-11-07 23:32:58 +0900

    2019.11.07

--- AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/AddProject.java
+++ AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/AddProject.java
@@ -2,6 +2,7 @@ package com.example.appdevelopment.ui.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,15 +12,19 @@ import com.example.appdevelopment.R;
 
 public class AddProject extends AppCompatActivity {
     Button button1;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project);
-        EditText editText = (EditText) findViewById(R.id.add_member_textview);
+        editText = (EditText) findViewById(R.id.add_member_textview);
         button1 = (Button)findViewById(R.id.add_member_button);
     }
 
     public void onClick(View view){
-
+        Intent intent = new Intent();
+        intent.putExtra("string", editText.getText());
+        setResult(0, intent);
+        finish();
     }
 }
--- AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/ProjectFragment.java
+++ AppDevelopment/app/src/main/java/com/example/appdevelopment/ui/project/ProjectFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,15 +21,23 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.appdevelopment.MainActivity;
 import com.example.appdevelopment.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectFragment extends Fragment implements View.OnClickListener {
     Button button;
     Bundle bundle;
     Context context;
+    LinearLayout linear_project;
+    LinearLayout.LayoutParams lp;
+    Button button_list;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_project, container, false);
         context = container.getContext();
         button = (Button)view.findViewById(R.id.add);
         button.setOnClickListener(this);
+        linear_project = (LinearLayout)view.findViewById(R.id.linear_project);
+        lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         return view;
     }
 
@@ -37,14 +46,20 @@ public class ProjectFragment extends Fragment implements View.OnClickListener {
         switch(v.getId()){
             case R.id.add:
                 Intent intent = new Intent(getContext(), AddProject.class);
-                startActivity(intent);
-
-//                NewProjectFragment projectFragment = new NewProjectFragment();
-//                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-//                fragmentTransaction.replace(R.id., projectFragment);
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
+                startActivityForResult(intent, 0);
                 break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0){
+            String result = data.getStringExtra("string");
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+            Button but = new Button(context);
+            but.setText(result);
+            linear_project.addView(but, lp);
         }
     }
 }
